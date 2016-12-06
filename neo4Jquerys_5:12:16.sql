3eaefea8b3119e11a1f01ffea8786c58d19d5d2d
Kevin Campuzano <kevincamp_90@me.com>
2016-12-05 23:58:46 -0500

    update actions query

--- neo4Jquerys_5:12:16.sql
+++ neo4Jquerys_5:12:16.sql
@@ -30,8 +30,28 @@ start r=rel(id) delete r;
 
 [{"id":"207af868.b1a418","type":"neo4j","z":"a1407d8a.ce131","name":"INFO NODE","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (a:Action {code:{code} })\nRETURN a","x":442,"y":309.76422119140625,"wires":[["8324463.ab4d3b8","f2fc9981.8fd4b"]]}]
 
-[{"id":"4e20926b.a5a7dc","type":"neo4j","z":"bc9bf4a0.4820e","name":"ONLY_OBJECTS2","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (device{code:{code}})-->(object)<-->(objects)MATCH (objects)<-[:HAVE]->(personals)RETURN  device,object,objects,personals","x":486,"y":792,"wires":[["9c63a845.8df84"]]}]
+[{"id":"4e20926b.a5a7dc","type":"neo4j","z":"bc9bf4a0.4820e","name":"ONLY_OBJECTS2","url":"http://neo4j:integradora@127.0.0.1:7474","query":
+"MATCH (device{code:{code}})-->(object)<-->(objects)
+MATCH (objects)<-[:HAVE]->(personals)
+RETURN  device,object,objects,personals",
+"x":486,"y":792,"wires":[["9c63a845.8df84"]]}]
 
 //DEVICE - OBJECT - ACTION
 [{"id":"4e20926b.a5a7dc","type":"neo4j","z":"bc9bf4a0.4820e","name":"ONLY_OBJECTS2","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (object{code:{code}})-[:HAVE]->(action) MATCH (object{code:{code}})<-[:HAVE]-(device) RETURN  device,object,action","x":486,"y":792,"wires":[["9c63a845.8df84"]]}]
 
+[{"id":"3509816a.e0ecce","type":"neo4j","z":"bc9bf4a0.4820e","name":"ONLY OBJECTS","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (device{code:{code}})-[:HAVE]->(object)<-[:link]->(objects)RETURN  object, objects","x":472,"y":1015,"wires":[["fd66356c.7ab8b8"]]}]
+
+// DEVICE - OBJECTS
+[{"id":"3509816a.e0ecce","type":"neo4j","z":"bc9bf4a0.4820e","name":"DEVICE_OBJECTS","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (device{code:{code}})-[:HAVE]->(object)<-[:link]->(objects) RETURN  {object: object, Objects: objects} as objetos","x":472,"y":1015,"wires":[["fd66356c.7ab8b8"]]}]
+MATCH (device{code:"000011"})-[:HAVE]->(object)<-[:link]->(objects) RETURN  {object: object, Objects: objects} as objetos
+
+// ACTION(ID,STATUS) -> OBJECTS
+[{"id":"3509816a.e0ecce","type":"neo4j","z":"bc9bf4a0.4820e","name":"DEVICE_OBJECTS","url":"http://neo4j:integradora@127.0.0.1:7474","query":"MATCH (device{code:{code},Status:{status}})<-[:HAVE]-(object)-[:link]->(objects) RETURN  {object: object,objects: objects}","x":472,"y":1015,"wires":[["fd66356c.7ab8b8"]]}]
+MATCH (device{code:"000017",Status:"ON"})<-[:HAVE]-(object)-[:link]->(objects)
+RETURN  {object: object,objects: objects}
+
+// ACTION -> CHANGES ACTIONS
+[{"id":"3509816a.e0ecce","type":"neo4j","z":"bc9bf4a0.4820e","name":"UPDATE_ACTIONS","url":"http://neo4j:integradora@127.0.0.1:7474", "query":"MATCH (device{code:{code}})<-[:HAVE]-(object)-[:link]->(objects)-[:HAVE]->(actions) SET actions.Status = {status} RETURN  object, objects, actions", "x":472,"y":1015,"wires":[["fd66356c.7ab8b8"]]}]
+MATCH (device{code:"000017",Status:"OFF"})<-[:HAVE]-(object)-[:link]->(objects)-[:HAVE]->(actions)
+SET actions.Status = "OFF"
+RETURN  object, objects, actions
